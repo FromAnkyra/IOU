1255e037f708f88d304e76db01ac380a065271ea
FromAnkyra <37625186+FromAnkyra@users.noreply.github.com>
2018-06-20 14:23:02 +0100

    Update oweytable.py

--- oweytable.py
+++ oweytable.py
@@ -4,18 +4,18 @@ users = sqlite.connect('whatever.db')
 class oweytable():
     
     def __init__(self, users):
-        //initialise the matrix based on the users, set all values to 0
-        //owings
+        #initialise the matrix based on the users, set all values to 0
+        #owings
         
     set_owing(userAid, userBid, amount):
-        //owings(userAid, userBid) += amount
-        //owings(userAid, userBid) += -amount
+        #owings(userAid, userBid) += amount
+        #owings(userAid, userBid) += -amount
         
     read(personAid):
-        //personAid = whatever
-        //for line in owings:
-        //  personAOwes.add(line(personAid))
-        //return personAOwes
+        #personAid = whatever
+        #for line in owings:
+        #  personAOwes.add(line(personAid))
+        #return personAOwes
     
     read(personAid, personBid):
-        //return owings(personAid, personBid)
+        #return owings(personAid, personBid)
