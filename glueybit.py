26b06356bd8549ac3bb1f9367b94ca96c9b7a023
FromAnkyra <37625186+FromAnkyra@users.noreply.github.com>
2018-06-20 14:22:12 +0100

    Create glueybit.py

---
+++ glueybit.py
@@ -0,0 +1,11 @@
+import oweytable
+import userdb
+#import whatever the slack api stuff I need
+
+#get the events - once the event is stored into a string (message), run this
+
+if message == '!IOU' + Auserid + ',' + Buserid:
+    sendmessage(Auserid + 'owes' Buserid + oweytable.read(Auserid, Buserid))
+elif message == '!IOU' + Auserid
+    #every non-zero owage
+elif message == 'IOU' + Auserid + ',' Buserid + ',' + #numbers
